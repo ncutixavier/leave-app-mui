b980f0eff23f64dbb40b2e0668f52592af100fde
ncutixavier <ncuti60@gmail.com>
2022-06-12 01:33:55 +0200

    add leave table and stats

---
+++ src/pages/employee/Home.js
@@ -0,0 +1,99 @@
+import * as React from "react";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { Box, Grid, Container, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import RotateLeftIcon from "@mui/icons-material/RotateLeft";
+import Leaves from "./Leaves";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  padding: theme.spacing(2),
+  color: theme.palette.text.secondary,
+}));
+
+const statsData = [
+  {
+    title: "Used Days",
+    value: "3",
+    color: "primary",
+  },
+  {
+    title: "Remaining Days",
+    value: "15",
+    color: "primary",
+  },
+];
+
+export default function Home() {
+  const theme = useTheme();
+
+  return (
+    <Container sx={{ flexGrow: 1, mt: 3, minWidth: "100%" }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 12 }}
+        columns={{ xs: 12, sm: 8, md: 12 }}
+        justifyContent="space-between"
+      >
+        {statsData.map((data, index) => (
+          <Grid item xs={12} sm={4} md={4} key={index}>
+            <Item
+              elevation={0}
+              variant="outlined"
+              sx={{
+                backgroundColor: theme.palette.secondary.main,
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography variant="h5">{data.value}</Typography>
+                <RotateLeftIcon fontSize="medium" color="primary" />
+              </Box>
+              <Typography
+                variant="p"
+                sx={{ color: theme.palette.primary.main }}
+              >
+                {data.title}
+              </Typography>
+            </Item>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container sx={{ mt: 2 }}>
+        <Grid item xs={12} sm={12} md={12}>
+          <Paper
+            elevation={0}
+            variant="outlined"
+            sx={{ padding: 2, mb: 2 }}
+          ></Paper>
+          <Leaves />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
---
+++ src/pages/employee/Leaves.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+function createData(date, days, leave, status) {
+  return { date, days, leave, status };
+}
+
+const rows = Array.from({ length: 50 }).map((_, index) => {
+  return createData("From 12 May - 25 May", "5", "Vacation", "Pending");
+});
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.primary.main,
+    textTransform: "uppercase",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    color: theme.palette.secondary.text,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(even)": {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    borderBottom: `3px solid ${theme.palette.primary.main}`,
+  },
+  height: "45px",
+}));
+
+const StyledTableHeader = styled(TableHead)(({ theme }) => ({
+  height: "45px",
+}));
+
+const Leaves = () => {
+  return (
+    <TableContainer
+      component={Paper}
+      elevation={0}
+      sx={{
+        maxHeight: {
+          md: "calc(100vh - 35vh)",
+          sm: "calc(100vh - 24px)",
+        },
+      }}
+    >
+      <Table
+        stickyHeader
+        sx={{ minWidth: 650 }}
+        size="small"
+        aria-label="simple table"
+      >
+        <StyledTableHeader>
+          <TableRow>
+            <StyledTableCell>Date</StyledTableCell>
+            <StyledTableCell>Days</StyledTableCell>
+            <StyledTableCell>Leave</StyledTableCell>
+            <StyledTableCell>Status</StyledTableCell>
+            <StyledTableCell align="center"></StyledTableCell>
+          </TableRow>
+        </StyledTableHeader>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row._id}>
+              <StyledTableCell>{row.date}</StyledTableCell>
+              <StyledTableCell>{row.days}</StyledTableCell>
+              <StyledTableCell>{row.leave}</StyledTableCell>
+              <StyledTableCell>{row.status}</StyledTableCell>
+              <StyledTableCell align="center">
+                <IconButton color="error">
+                  <DeleteIcon />
+                </IconButton>
+                <IconButton color="primary">
+                  <EditIcon />
+                </IconButton>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Leaves;
